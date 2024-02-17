c6f64bbb0c9c307dcdbfed59082829af683d87b5
Shashwat <anshuphysic07@gmail.com>
2024-02-18 00:47:34 +0530

    tryandtry

---
+++ script.js
@@ -0,0 +1,25 @@
+// let a=prompt("enter first num");
+// let b=Number(prompt("enter second num"));
+// alert(Number(a)+(b));
+console.dir(document.body);
+
+const subtract=()=>{
+    let a=parseInt(prompt("enter first number"));
+    let n=parseInt(prompt("enter second number"));
+    alert(a-n);
+}
+const multiply=()=>{
+    let a=parseInt(prompt("enter first number"));
+    let n=parseInt(prompt("enter second number"));
+    alert(a*n);
+}
+const division=()=>{
+    let a=parseInt(prompt("enter first number"));
+    let n=parseInt(prompt("enter second number"));
+    alert(a/n);
+}
+const add=function(){
+  let a=parseInt(prompt("enter first number"));
+  let n=parseInt(prompt("enter second number"));
+  alert(a+n);
+}
